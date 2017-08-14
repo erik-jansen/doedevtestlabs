b48104c5d346211fccc285cf3a72fc82101ca47f
Erik Jansen <ej@live.nl>
2017-08-15 00:42:42 +1000

    Scripts

--- Scripts/generateFile.ps1
+++ Scripts/generateFile.ps1
@@ -1,1 +1,4 @@
-New-Item c:\scripts\new_file.txt -type file
+param([string]$var1='',[string]$var2='')
+
+New-Item c:\scripts -type directory
+New-Item c:\scripts\new_file.txt -type file -value "var1=$var1 and var2=$var2"
